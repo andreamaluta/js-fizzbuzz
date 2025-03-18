5605e3175cf87cb1434d4e9a88fc32c262c14f7c
andreamaluta <andreamaluta@gmail.com>
2025-03-18 14:22:32 +0100

    [UPD] created the structor of the if

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // scrivo il ciclo per stampare tutti i numeri da 1 a 100
 for(let i=1; i<=100; i++){
+    
+    // metto le condizioni per controllare se divisibile
+    // controllo se è divisibile per 3
+    if(i % 3 == 0)
+        {
+
+    }
+    // controllo se è divisibile per 5
+    else if(i % 5 == 0){
+
+    }
+    // controllo se è divisibile sia per 3 che per 5
+    else if(i % 3 == 0 && i % 5 == 0){
+
+    }
+    // se non è divisibile ne per 3 ne per 5
+    else{
+
+    }
 
     console.log(i);
 
