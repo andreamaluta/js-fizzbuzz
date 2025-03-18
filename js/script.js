373703204697a6ac1d6d98d87f7719f93f7ae835
andreamaluta <andreamaluta@gmail.com>
2025-03-18 14:26:48 +0100

    [DONE] esercizio finito

--- js/script.js
+++ js/script.js
@@ -3,23 +3,21 @@ for(let i=1; i<=100; i++){
     
     // metto le condizioni per controllare se divisibile
     // controllo se è divisibile per 3
-    if(i % 3 == 0)
-        {
-
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        console.log("FizzBuzz");
     }
     // controllo se è divisibile per 5
     else if(i % 5 == 0){
-
+        console.log("Buzz");
     }
     // controllo se è divisibile sia per 3 che per 5
-    else if(i % 3 == 0 && i % 5 == 0){
-
+    else if(i % 3 == 0){
+        console.log("Fizz");
     }
     // se non è divisibile ne per 3 ne per 5
     else{
-
+        console.log(i);
     }
-
-    console.log(i);
 
 }
